bfacb3ba4400508785532b2d619a00304f602426
serik <podelu000@gmail.com>
2023-07-11 19:23:38 +0300

    успешно

--- src/lib.rs
+++ src/lib.rs
@@ -28,12 +28,12 @@ mod tests {
     fn one_result() {
         let query = "duct";
         let contents = "\
-        \
-        Rust:\
-        safe, fast, productive.\
-        Pick three.";
 
-        assert_eq!(vec!["save, fast, productive."], search(query, contents));
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
     pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
         let mut result = Vec::new();
