2814c989bfbade60d4ddc88021fe7476f946713b
serik <podelu000@gmail.com>
2023-07-11 19:33:44 +0300

    готово

--- src/lib.rs
+++ src/lib.rs
@@ -17,12 +17,16 @@ impl Config {
 }
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let contents = fs::read_to_string(config.filename)?;
-    println!("С текстом: \n{}", contents);
+
+    for line in search(&config.query, &contents){
+        println!("{}", line);
+    }
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
     #[test]
     fn one_result() {
@@ -35,14 +39,14 @@ Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
-    pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-        let mut result = Vec::new();
+}
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
 
-        for line in contents.lines() {
-            if line.contains(query) {
-                result.push(line);
-            }
+    for line in contents.lines() {
+        if line.contains(query) {
+            result.push(line);
         }
-        result
     }
+    result
 }
