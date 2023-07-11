6161c3b8bb7c9e403bedecb7b27352a51377a2b3
serik <podelu000@gmail.com>
2023-07-11 23:12:54 +0300

    итераторный стиль для поиса

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,17 @@ pub struct Config {
     pub case_sensitive: bool,
 }
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Недостаточно аргументов");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
+    pub fn new(mut args: std::env::Args) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Не получена строка запроса"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Не получено имя файла"),
+        };
 
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
 
@@ -35,26 +40,15 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            result.push(line);
-        }
-    }
-    result
+    contents.lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut result = Vec::new();
-    let query = query.to_lowercase();
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(&query) {
-            result.push(line);
-        }
-    }
-    result
+    contents.lines()
+        .filter(|lines| lines.contains(query))
+        .collect()
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err|{
+    let config = Config::new(env::args()).unwrap_or_else(|err|{
         eprintln!("Проблема при разборе аргументов: {}", err);
         process::exit(1);
     });
 
-    println!("Поиск: {}", config.query);
-    println!("В файле: {}", config.filename);
+    println!("Search: {}", config.query);
+    println!("In file: {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
         eprintln!("Ошибка в приложении: {}", e);
